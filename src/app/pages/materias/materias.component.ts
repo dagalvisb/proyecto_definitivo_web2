a165302a5f05b07afc93ab6ec393d5d5b13d3e31
dagalvisb <daniel.galvis42587@ucaldas.edu.co>
2025-09-10 12:53:12 -0500

    formulario materias

--- src/app/pages/materias/materias.component.ts
+++ src/app/pages/materias/materias.component.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-materias',
-  imports: [],
-  templateUrl: './materias.component.html',
-  styleUrl: './materias.component.css'
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './materias.component.html'
 })
 export class MateriasComponent {
+  materiasForm: FormGroup;
+  materiasDisponibles: string[] = ['Matemáticas', 'Física', 'Programación', 'Historia', 'Inglés'];
 
+  constructor(private fb: FormBuilder) {
+    this.materiasForm = this.fb.group({
+      codigo: ['', [Validators.required, Validators.minLength(4)]],
+      nombre: ['', Validators.required],
+      materia: ['', Validators.required],
+      semestre: ['', [Validators.required, Validators.pattern('^[0-9]+$')]],
+    });
+  }
+
+  onSubmit(): void {
+    if (this.materiasForm.valid) {
+      console.log('Formulario enviado ✅', this.materiasForm.value);
+      alert('Inscripción realizada con éxito');
+      this.materiasForm.reset();
+    } else {
+      console.log('Formulario inválido ❌');
+    }
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,14 @@ import { provideRouter, withPreloading, PreloadAllModules } from '@angular/route
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
+import { ReactiveFormsModule } from '@angular/forms';
+import { importProvidersFrom } from '@angular/core';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes, withPreloading(PreloadAllModules)),
     provideHttpClient(withFetch()),
+    importProvidersFrom(ReactiveFormsModule)
   ],
 }).catch(err => console.error(err));
+
